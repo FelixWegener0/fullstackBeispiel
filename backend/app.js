a12261e1d71448ce9560c978ce66d8925ec5dc72
Felix Wegener <Felix.Wegener05@t-online.de>
2024-06-28 16:30:40 +0200

    add backend nodejs error handling

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors'
 
-import { addNewUser, getAllUser, connectDatabase } from './database/userFunctions.mjs';
+import { addNewUser, getAllUser, connectDatabase, closeConnectionDatabase } from './database/userFunctions.mjs';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const server = app.listen(4000, () => {
+    console.log('node server started');
+});
+
+connectDatabase();
 
 app.get('/', (req, res) => {
     res.send('Hello word this is a node server :D');
@@ -20,7 +26,36 @@ app.get('/getAllUser', (req, res) => {
     getAllUser().then(result => res.send(result)).catch(error => res.send('error' + error));
 });
 
-connectDatabase();
-app.listen(4000, () => {
-    console.log('node server started');
+
+// Error handling
+function onShutdown() {
+    console.log('shuting down server');
+    closeConnectionDatabase();
+    server.close(() => {
+        console.log('server closed');
+        process.exit();
+    });
+};
+
+// SIGINT (Strg+C)
+process.on('SIGINT', () => {
+    console.log('SIGINT erhalten');
+    onShutdown();
+});
+
+// SIGTERM
+process.on('SIGTERM', () => {
+    console.log('SIGTERM erhalten');
+    onShutdown();
+});
+
+// unknown
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    onShutdown();
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection:', reason);
+    onShutdown();
 });

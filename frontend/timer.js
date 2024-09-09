f0d60da8718944162303b4331998708394ee9cc5
Felix Wegener <Felix.Wegener05@t-online.de>
2024-09-09 19:37:42 +0200

    add pause button to timer

--- frontend/timer.js
+++ frontend/timer.js
@@ -4,20 +4,25 @@ let initTimeHours = 3;
 let addedminutes = 10;
 
 let timer = localStorage.getItem("timer") || (initTimeHours * 3600);
+let timerPaused = false;
 
 let timerComponent = document.getElementById("timerComponent");
 let addTimeButton = document.getElementById("addMinuteButton");
 let restartTimer = document.getElementById("restartTimer");
+let pauseTimer = document.getElementById("pauseTimer");
 
 const formatTimer = (input) => {
     let value;
-
-    if (input < 10) {
-        value = "0" + input;
-    } else {
-        value = input;
-    }
+    input < 10 ? value = "0" + input : value = input;
     return value;
+}
+
+const detectPause = () => {
+    if (timerPaused) {
+        return "PAUSED"
+    } else {
+        return ""
+    }
 }
 
 const timerIntervall = setInterval(() => {
@@ -26,15 +31,14 @@ const timerIntervall = setInterval(() => {
     let minutes = restSeonds / 60;
     let seconds = restSeonds % 60;
 
-    timerComponent.innerHTML = `Timer: ${formatTimer(Math.floor(houres))}:${formatTimer(Math.floor(minutes))}:${formatTimer(seconds)}`;
+    timerComponent.innerHTML = `Timer: ${formatTimer(Math.floor(houres))}:${formatTimer(Math.floor(minutes))}:${formatTimer(seconds)} ${detectPause()}`;
     localStorage.setItem("timer", timer);
 
-    if (timer <= 0) {
-        timer = 0;
-    } else {
-        timer--;
+    if (!timerPaused) {
+        timer <= 0 ? timer = 0 : timer--;
     }
 }, 999);
 
 restartTimer.addEventListener("click", () => timer = initTimeHours * 3600);
 addTimeButton.addEventListener("click", () => timer = parseInt(timer) + parseInt(60 * addedminutes));
+pauseTimer.addEventListener("click", () => { timerPaused = !timerPaused });
